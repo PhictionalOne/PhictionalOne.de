83e771ded1e88e2db7d55e0935e61dcde85968d8
PhictionalOne <code@phictional.de>
2020-10-26 21:35:02 +0100

    Update CSS
    Introduce new dark color and material-ish design

--- index.php
+++ index.php
@@ -16,10 +16,9 @@
 
 <body>
     <div class="bgimg w3-display-container w3-animate-opacity w3-text-white noselect">
-        <div class="w3-display-bottomleft w3-padding-large w3-xlarge">
+        <div class="w3-display-bottomleft w3-padding-large w3-xlarge impressum">
             <span class="w3-tooltip">
                 <img src="Logo.svg" alt="φctional Logo" class="icon" width="20" height="20"/>
-
                 <span class="w3-text w3-small">
                   <a href="impressum.html" style="text-decoration: none">Impressum &amp; Datenschutz</a>
                 </span>
@@ -28,22 +27,22 @@
         </div>
 
         <!-- Center Stuff: Logo & Title -->
-        <div class="w3-display-middle" style="cursor:default">
-            <svg class="svg w3-animate-top" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="400" height="516.8674698795181" viewBox="0, 0, 400,516.8674698795181">
+        <div class="w3-display-middle middle" style="cursor:default">
+            <svg class="svg" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="400" height="516.8674698795181" viewBox="0, 0, 400,516.8674698795181">
               <g class="svg">
                 <path id="one" d="M99.699 105.489 L 0.000 155.425 0.000 175.303 C 0.000 186.236,0.327 195.181,0.727 195.181 C 1.127 195.181,41.872 175.120,91.272 150.602 C 140.672 126.084,181.278 106.024,181.509 106.024 C 181.739 106.024,181.928 196.447,181.928 306.965 L 181.928 507.905 190.964 512.377 L 200.000 516.848 209.036 512.377 L 218.072 507.905 218.072 286.145 L 218.072 64.384 209.016 59.903 C 204.035 57.438,199.834 55.452,199.679 55.488 C 199.524 55.524,154.533 78.025,99.699 105.489 " stroke="none" fill="#000000" fill-rule="evenodd"></path>
 
                 <path id="phi" d="M190.663 4.552 L 181.928 8.960 181.928 24.360 C 181.928 32.829,182.212 39.759,182.560 39.759 C 182.908 39.759,186.974 37.855,191.596 35.527 L 200.000 31.295 208.404 35.527 C 213.026 37.855,217.092 39.759,217.440 39.759 C 217.788 39.759,218.072 32.830,218.072 24.361 L 218.072 8.962 209.016 4.481 C 204.035 2.016,199.834 0.032,199.679 0.072 C 199.524 0.111,195.467 2.127,190.663 4.552 M242.169 97.298 L 242.169 117.488 306.024 149.398 L 369.880 181.307 369.880 263.855 L 369.880 346.404 306.024 378.313 L 242.169 410.223 242.169 430.413 C 242.169 441.517,242.441 450.602,242.775 450.602 C 243.108 450.602,278.620 432.981,321.690 411.444 L 400.000 372.286 400.000 263.855 L 400.000 155.425 321.690 116.267 C 278.620 94.730,243.108 77.108,242.775 77.108 C 242.441 77.108,242.169 86.194,242.169 97.298 M14.759 212.483 L 0.000 219.834 0.000 296.060 L -0.000 372.286 78.310 411.444 C 121.380 432.981,156.892 450.602,157.225 450.602 C 157.559 450.602,157.831 441.517,157.831 430.413 L 157.831 410.223 93.985 378.318 L 30.139 346.413 29.829 275.773 L 29.518 205.133 14.759 212.483 " stroke="none" fill="#000000" fill-rule="evenodd"></path>
               </g>
             </svg>
-            <h1 class="w3-xxxlarge w3-animate-top">Phictional<e class="emp">One</e></h1>
-            <hr class="w3-border-grey" style="margin:auto;width:40%">
-            <p class="w3-large w3-center">Hier entsteht <e class="emp">irgend</e>wann <e class="emp">irgend</e>was</p>
+            <h1 class="w3-xxxlarge">Phictional<span class="emp">One</span></h1>
+            <hr class="w3-border-grey divider">
+            <p class="w3-large w3-center">Hier entsteht <span class="empp">irgend</span>wann <span class="empp">irgend</span>was</p>
 
         </div>
 
         <!-- Social & Contact Information/Buttons -->
-        <div class="w3-display-bottomright w3-padding-large w3-xlarge">
+        <div class="w3-display-bottomright w3-padding-large w3-xlarge social">
             <span id="Mail" class="icon w3-tooltip">
                 <span class="w3-text"><a href="pgp-key.txt" target="_blank">
                         <i class="fas fa-key icon" title="PGP"></i></a></span>
